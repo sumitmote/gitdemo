6c60201f3062225aa886dbbeab5e6dbb65cdb9f1
sumitmote <sumitmote19@gmail.com>
2023-09-12 11:30:32 +0530

    conflict resolved and commited

--- src/my/git/demo/MyFirstGit.java
+++ src/my/git/demo/MyFirstGit.java
@@ -5,6 +5,8 @@ public class MyFirstGit {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("I am sumit.");
+		System.out.println("I am searching job in india");
+		System.out.println("I am sanjeet from bihar..");
 	}
 	
 }
