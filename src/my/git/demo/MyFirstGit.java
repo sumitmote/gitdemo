ffb3b3afaeef893a6ddc7fddd8479a535cf437bf
sumitmote <sumit.mote@aol.com>
2024-05-05 18:08:23 +0530

    "removed all sumit"

--- src/my/git/demo/MyFirstGit.java
+++ src/my/git/demo/MyFirstGit.java
@@ -3,12 +3,7 @@ package my.git.demo;
 public class MyFirstGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("I am sumit.");
-		System.out.println("I am searching job in india");
-		System.out.println("I am sanjeet from bihar..");
-		System.out.println("change regarding sumit_dev");
-		System.out.println("Main regarding change..");
+
 	}
 	
 }
