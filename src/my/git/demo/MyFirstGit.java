47f53f14186286ed5fa759d6434bbd60e726ea69
sumitmote <sumit.mote@aol.com>
2024-05-05 17:39:40 +0530

    added changes regarding sout

--- src/my/git/demo/MyFirstGit.java
+++ src/my/git/demo/MyFirstGit.java
@@ -4,6 +4,7 @@ public class MyFirstGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Sanjeet change is commiting into the main branch");
 	}
 	
 }
