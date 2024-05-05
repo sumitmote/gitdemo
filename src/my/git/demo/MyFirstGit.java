752b4eca5d89682b349a527e10280a5ab79d73af
sumitmote <sumit.mote@aol.com>
2024-05-05 17:51:57 +0530

    removed all from MyFirstGit-sanjeet

--- src/my/git/demo/MyFirstGit.java
+++ src/my/git/demo/MyFirstGit.java
@@ -3,9 +3,7 @@ package my.git.demo;
 public class MyFirstGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		int number = 10;
-		System.out.println(number);
+		
 	}
 	
 }
