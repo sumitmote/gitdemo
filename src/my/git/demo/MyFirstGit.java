c9265e6b13fb5b0bb0553c12cae8dab95b910732
sumitmote <sumit.mote@aol.com>
2024-05-25 20:37:40 +0530

    commit_sanjeet added files

--- src/my/git/demo/MyFirstGit.java
+++ src/my/git/demo/MyFirstGit.java
@@ -3,8 +3,7 @@ package my.git.demo;
 public class MyFirstGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Sanjeet change is commiting into the main branch");
+		
 	}
 	
 }
